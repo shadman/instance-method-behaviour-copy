2dec3349f05ad49a687b5be5525191225c1c8281
Shadman <shadman.jamil@tenpearls.com>
2016-09-06 01:06:17 +0500

    removed to_s that was placed for testing

--- instance_method.rb
+++ instance_method.rb
@@ -4,7 +4,6 @@ class Array
    self.map {|e| yield e }
   end
 
-
   def a_map(&block)
     map(&block)
   end
@@ -12,20 +11,20 @@ class Array
 end
 
 
+# Examples 
 
 a=[1,2,3,4]
 
 puts "\nExamples for a.map(&:to_s)"
 
 puts "\nNormal `map` behaviour:"
-puts a.map(&:to_s).to_s   # Converting to string for a same response as we get in console
+puts a.map(&:to_s)
 
+puts "\Overriding `map` with yield in `my_app`:"
+puts a.my_map(&:to_s)
 
-puts "\nOverwritting `map` with yield in `my_app`:"
-puts a.my_map(&:to_s).to_s  # Converting to string for a same response as we get in console
-
-puts "\nOverwritting by reference value in `a_map`:"
-puts a.a_map(&:to_s).to_s   # Converting to string for a same response as we get in console
+puts "\Overriding by reference value in `a_map`:"
+puts a.a_map(&:to_s)
 
 
 puts "\n\n"
